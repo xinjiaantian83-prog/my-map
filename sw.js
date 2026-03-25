400e476fcd0fd76281783472a274cd4bcfb6bf0c
xinjiaantian83-prog <xinjiaantian83@gmail.com>
2026-03-25 15:12:10 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
-self.addEventListener('install', (e) => {
+self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request, { cache: 'no-store' }).catch(() => caches.match(event.request))
+  );
 });
